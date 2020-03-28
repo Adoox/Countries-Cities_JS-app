fc9f7480ee82758899e9292948b34e046d1e774f
Adnan <adnankubat40@gmail.com>
2020-03-28 16:33:19 +0100

    First commit

---
+++ europe_learning.js
@@ -0,0 +1,41 @@
+const search_country = document.querySelector(".search_country");
+const search_country_btn = document.querySelector("#search_country_btn");
+const flag_box = document.querySelector(".flag_box");
+const country_name = document.querySelector("#country_name");
+const capital_city = document.querySelector("#capital_city");
+const region = document.querySelector("#region");
+const subregion = document.querySelector("#subregion");
+const population = document.querySelector("#population");
+
+const informations = {};
+
+$(search_country_btn).click(function() {
+  let country_api = `https://restcountries.eu/rest/v2/name/${search_country.value}`; /*dohvacanje podataka uz pomoc vrijednosti drzave koju korisnik unese*/
+
+  fetch(country_api)
+    .then(function(response) {
+      let data = response.json();
+      return data;
+    })
+    .then(function(data) {
+      console.log(data);
+      informations.flag = data[0].flag;
+      informations.country_name = data[0].name;
+      informations.capital_city = data[0].capital;
+      informations.region = data[0].region;
+      informations.subregion = data[0].subregion;
+      informations.population = data[0].population;
+    })
+    .then(function() {
+      displayAllInformation();
+    });
+});
+
+function displayAllInformation() {
+  flag_box.innerHTML = `<img src="${informations.flag}"/>`;
+  country_name.innerHTML = informations.country_name;
+  capital_city.innerHTML = `<span id="span">Capital city: </span>${informations.capital_city}`;
+  region.innerHTML = `<span id="span">Region: </span>${informations.region}`;
+  subregion.innerHTML = `<span id="span">Subregion: </span>${informations.subregion}`;
+  population.innerHTML = `<span id="span">Population: </span>${informations.population}`;
+}
---
+++ srcipt2.js
@@ -0,0 +1,71 @@
+const click_btn = document.querySelector("#button1");
+const check_btn = document.querySelector("#button2");
+const flag_place = document.querySelector(".flag_place");
+const country_name = document.querySelector(".country_name p");
+const input = document.querySelector(".input_place");
+
+const information = {};
+
+$(click_btn).click(function() {
+  random = Math.floor(
+    Math.random() * 54
+  ); /*generisanje random broja koji ce predstavljati odredenu drzavu iz niza*/
+  //let api = "https://restcountries.eu/rest/v2/all";
+  /*let api = `https://restcountries.eu/rest/v2/name/Croatia`;*/
+  let api = `https://restcountries.eu/rest/v2/region/europe`;
+
+  fetch(api)
+    .then(function(response) {
+      let data = response.json();
+      return data;
+    })
+    .then(function(data) {
+      console.log(data);
+      information.country_flag = data[random].flag;
+      information.name = data[random].name;
+      capital_city = data[random].capital;
+      capital_city_lowerCase = capital_city.toLowerCase();
+      slova = capital_city_lowerCase.split("");
+      console.log(capital_city);
+      console.log(slova);
+      console.log(slova[0]);
+    })
+    .then(function() {
+      displayInformation();
+    })
+    .then(function() {
+      $(input).val(""); /*resertovanje polja za unos*/
+      click_btn.setAttribute("disabled", "disabled");
+      $(click_btn).css("border", "none");
+    });
+});
+
+$(check_btn).click(function() {
+  /*funckija za provjeru ispravnosti unesenog glavmog grada*/
+  console.log(random);
+  console.log(capital_city);
+  input_value = input.value;
+  input_value_lowerCase = input_value.toLowerCase();
+  result = input_value_lowerCase.localeCompare(
+    capital_city_lowerCase
+  ); /*uporeduje unos korisnika i vrijednost glavnog grada za odredenu drzavu*/
+  if (result == 0) {
+    console.log("true");
+    click_btn.removeAttribute(
+      "disabled"
+    ); /*postavljanje vrijednosti "enable" na button kada je odgovor tacan*/
+    $(click_btn).css("border", "3px solid #1ce73d");
+  } else {
+    console.log("false");
+    click_btn.setAttribute(
+      "disabled",
+      "disabled"
+    ); /*postavljanje vrijednosti "disable" na button kada je odgovor netacan*/
+    $(click_btn).css("border", "3px solid red");
+  }
+});
+
+function displayInformation() {
+  flag_place.innerHTML = `<img src="${information.country_flag}"/>`;
+  country_name.innerHTML = `${information.name}`;
+}
